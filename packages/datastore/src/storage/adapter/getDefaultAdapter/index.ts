512536b246ef5203fe704b8d6d0bb4ad412d409e
Armen Rostamian <armenr@gmail.com>
2021-08-30 11:45:04 -0700

    Update index.ts

--- packages/datastore/src/storage/adapter/getDefaultAdapter/index.ts
+++ packages/datastore/src/storage/adapter/getDefaultAdapter/index.ts
@@ -1,14 +1,14 @@
 import { browserOrNode, isWebWorker } from '@aws-amplify/core';
 import { Adapter } from '..';
+import * as IndexedDBAdapter from '../IndexedDBAdapter'
+import { AsyncStorageAdapter } from '../AsyncStorageAdapter'
 
 const getDefaultAdapter: () => Adapter = () => {
 	const { isBrowser } = browserOrNode();
 
 	if ((isBrowser && window.indexedDB) || (isWebWorker() && self.indexedDB)) {
-		return require('../IndexedDBAdapter').default;
+    return IndexedDBAdapter.default
 	}
-
-	const { AsyncStorageAdapter } = require('../AsyncStorageAdapter');
 
 	return new AsyncStorageAdapter();
 };
